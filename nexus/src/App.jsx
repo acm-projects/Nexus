607b7438f7f0df7c92ed04d519b534affc01cc58
Tomm4y <aeonxofc@gmail.com>
2024-10-18 15:59:13 -0500

    navbar blur when scrolled

--- nexus/src/App.jsx
+++ nexus/src/App.jsx
@@ -28,7 +28,6 @@ function App() {
           />
           <Route path="/login" element={<LoginPage />} />
           <Route path="/register" element={<RegisterPage />} />
-          
           <Route path="/doc-preview" element={<DocPreview />} />
         </Routes>
       </div>
